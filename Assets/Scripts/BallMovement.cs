a017ed8f30bf433a48a06f142ccd36a25c65ba42
PedroR <pedrorua98@hotmail.com>
2020-02-29 15:13:18 +0000

    Better bounciness

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,31 +5,51 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     GameObject ball;
+    bool physics;
     void Start()
     {
         ball = this.gameObject;
+        ball.GetComponent<Rigidbody>().useGravity = false;
+        physics = false;
     }
 
     void Update()
     {
-        if (Input.GetKey("w"))
+        if (physics == true)
         {
-            ball.GetComponent<Rigidbody>().AddForce(new Vector3(3f, 0f, 0f));
-        }
+            ball.GetComponent<Rigidbody>().useGravity = true;
 
-        if (Input.GetKey("s"))
-        {
-            ball.GetComponent<Rigidbody>().AddForce(new Vector3(-3f, 0f, 0f));
-        }
+            if (Input.GetKey("w"))
+            {
+                ball.GetComponent<Rigidbody>().AddForce(new Vector3(3f, 0f, 0f));
+            }
 
-        if (Input.GetKey("a"))
-        {
-            ball.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, 3f));
-        }
+            if (Input.GetKey("s"))
+            {
+                ball.GetComponent<Rigidbody>().AddForce(new Vector3(-3f, 0f, 0f));
+            }
 
-        if (Input.GetKey("d"))
-        {
-            ball.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, -3f));
+            if (Input.GetKey("a"))
+            {
+                ball.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, 3f));
+            }
+
+            if (Input.GetKey("d"))
+            {
+                ball.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, -3f));
+            }
+
+            if (Input.GetKeyDown("space"))
+            {
+                ball.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, -500f));
+            }
         }
+    }
+
+    private void OnMouseDown()
+    {
+        // Destroy(gameObject);
+        ball.GetComponent<Rigidbody>().AddForce(new Vector3(450f, -200f, 50f));
+        physics = true;
     }
 }
