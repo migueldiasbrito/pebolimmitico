219b668dacaa83445742ea0a09138ebd9dd47362
Moises <liricractos@gmail.com>
2020-02-29 14:43:33 +0000

    My scene changed

---
+++ Assets/Scripts/rotateMatreco.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateMatreco : MonoBehaviour
+{
+
+    public int speed; 
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            this.GetComponent<Rigidbody>().AddTorque(0,0,-speed, ForceMode.VelocityChange);
+        }
+    }
+}
