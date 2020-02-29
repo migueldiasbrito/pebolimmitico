c41fd19015558360da5a614008fc9b36c0baf6b9
Moises <liricractos@gmail.com>
2020-02-29 17:47:39 +0000

    script move meco

--- Assets/Scripts/rotateMatreco.cs
+++ Assets/Scripts/rotateMatreco.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class rotateMatreco : MonoBehaviour
 {
-
-    //Make sure you attach a Rigidbody in the Inspector of this GameObject
-    Rigidbody m_Rigidbody;
-    Vector3 m_EulerAngleVelocity;
+    Rigidbody r;
+    public int torque;
 
     void Start()
     {
-        //Set the axis the Rigidbody rotates in (100 in the y axis)
-        m_EulerAngleVelocity = new Vector3(0, 0, -1000);
-
-        //Fetch the Rigidbody from the GameObject with this script attached
-        m_Rigidbody = GetComponent<Rigidbody>();
+        r = this.gameObject.GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.J))
         {
-            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
-            m_Rigidbody.AddTorque(new Vector3(0, 0, -1000), ForceMode.Impulse);
+            float turn = Input.GetAxis("Horizontal");
+            r.AddTorque(transform.forward * torque);
         }
-       
+        if (Input.GetKey(KeyCode.K))
+        {
+            float turn = Input.GetAxis("Horizontal");
+            r.AddTorque(-transform.forward * torque);
+        }
+
     }
 }
