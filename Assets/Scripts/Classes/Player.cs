a047cf73ad716202ffc046ac9c8a8bd51b8e784d
Moises <liricractos@gmail.com>
2020-02-29 22:44:26 +0000

    Class

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -6,16 +6,19 @@ public class Player
 {
     public Sprite spriteMain;
     public Sprite spriteLacaio1, spriteLacaio2;
-    public AudioClip bolaNoMatreco, bolaGolo;
+    public AudioClip bolaNoMatreco, bolaGolo, themesong;
 
     public float force, acuracy;
 
-    public Player(Sprite spriteMain, Sprite spriteLacaio1, Sprite spriteLacaio2, float force, float acuracy)
+    public Player(Sprite spriteMain, Sprite spriteLacaio1, Sprite spriteLacaio2, float force, float acuracy, AudioClip bolaNoMatreco, AudioClip bolaGolo, AudioClip themeSong)
     {
         this.spriteMain = spriteMain;
         this.spriteLacaio1 = spriteLacaio1;
         this.spriteLacaio2 = spriteLacaio2;
         this.force = force;
         this.acuracy = acuracy;
+        this.bolaNoMatreco = bolaNoMatreco;
+        this.bolaGolo = bolaGolo;
+        this.themesong = themeSong;
     }
 }
