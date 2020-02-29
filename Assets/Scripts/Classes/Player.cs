002b9c00a00e7354a8a9417ba3bf7d754027752c
Moises <liricractos@gmail.com>
2020-02-29 21:34:44 +0000

    Classe player

---
+++ Assets/Scripts/Classes/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player 
+{
+    public Sprite spriteMain;
+    public Sprite spriteLacaio1, spriteLacaio2;
+    public AudioClip bolaNoMatreco, bolaGolo;
+
+    public float force, acuracy;
+
+    public Player(Sprite spriteMain, Sprite spriteLacaio1, Sprite spriteLacaio2, float force, float acuracy)
+    {
+        this.spriteMain = spriteMain;
+        this.spriteLacaio1 = spriteLacaio1;
+        this.spriteLacaio2 = spriteLacaio2;
+        this.force = force;
+        this.acuracy = acuracy;
+    }
+}
