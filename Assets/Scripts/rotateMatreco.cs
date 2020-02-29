912716557656367fb9b74ef20a62947fc7d2756b
Moises <liricractos@gmail.com>
2020-02-29 14:58:32 +0000

    solved

--- Assets/Scripts/rotateMatreco.cs
+++ Assets/Scripts/rotateMatreco.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class rotateMatreco : MonoBehaviour
 {
 
-    public int speed; 
+    //Make sure you attach a Rigidbody in the Inspector of this GameObject
+    Rigidbody m_Rigidbody;
+    Vector3 m_EulerAngleVelocity;
+
+    void Start()
+    {
+        //Set the axis the Rigidbody rotates in (100 in the y axis)
+        m_EulerAngleVelocity = new Vector3(0, 0, -1000);
+
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKey(KeyCode.J))
         {
-            this.GetComponent<Rigidbody>().AddTorque(0,0,-speed, ForceMode.VelocityChange);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
         }
+       
     }
 }
