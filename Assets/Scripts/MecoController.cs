e77dffb51723775b4fd7b8259bc759b8ffb0bde2
Miguel Brito <migueldiasbrito@gmail.com>
2020-02-29 16:33:03 +0000

    lets get physical. physical.

--- Assets/Scripts/MecoController.cs
+++ Assets/Scripts/MecoController.cs
@@ -7,21 +7,25 @@ public class MecoController : MonoBehaviour
     public float range;
 
     private float m_origin;
+    private Rigidbody m_Rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         m_origin = transform.position.x;
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float velocity = Input.GetAxis(axis) * speed;
-        
-        transform.position = new Vector3(
+
+        /*transform.position = new Vector3(
         Mathf.Max(m_origin - range, Mathf.Min(m_origin + range, transform.position.x + velocity * Time.deltaTime)),
         transform.position.y,
-        transform.position.z);
+        transform.position.z);*/
+
+        m_Rigidbody.velocity = new Vector3(velocity, m_Rigidbody.velocity.y, m_Rigidbody.velocity.z);
     }
 }
