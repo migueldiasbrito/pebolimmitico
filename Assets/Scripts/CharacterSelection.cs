d4ea9cbf8ebf540a94cfe6e5dfd4983dee960c0e
PedroR <pedrorua98@hotmail.com>
2020-03-01 03:20:56 +0000

    Character Selection

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -1,23 +1,181 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
-    GameObject characters;
+    public GameObject chars;
+    public GameObject chars2;
+    private GameObject[] characters;
+    private GameObject[] characters2;
+    public GameObject player1;
+    public GameObject player2;
+    //
+    public Sprite[] spriterinos;
+    public Sprite[] spriterinos2;
+    Sprite spriten;
+    Sprite spriten2;
 
     void Start()
     {
-        characters = GetComponentInParent<>()
-        
+        characters = new GameObject[chars.transform.childCount];
+        characters2 = new GameObject[chars2.transform.childCount];
+
+        for (int i = 0; i < chars.transform.childCount; i++)
+        {
+            characters[i] = chars.transform.GetChild(i).gameObject;
+            characters2[i] = chars2.transform.GetChild(i).gameObject;
+        }
+
+        for(int i = 0; i < characters.Length; i++)
+        {
+            characters[i].GetComponentInChildren<CharacterSprite>().sprite = spriterinos[i];
+            characters2[i].GetComponentInChildren<CharacterSprite>().sprite = spriterinos2[i];
+        }
+
+        Debug.Log(characters[2]);
     }
 
     void Update()
     {
-        
+        player1.GetComponent<Image>().sprite = characters[2].GetComponentInChildren<CharacterSprite>().sprite;
+        player2.GetComponent<Image>().sprite = characters2[2].GetComponentInChildren<CharacterSprite>().sprite;
     }
 
-    void LeftButton()
+    public void LeftButton1()
+    {
+        for(int i = 0; i < characters.Length; i++)
+        {
+            if (i == 0)
+            {
+                spriten = spriterinos[0];
+                Debug.Log(spriten);
+            }
+
+            if (i != characters.Length - 1)
+            {
+                characters[i].GetComponentInChildren<CharacterSprite>().sprite = characters[i + 1].GetComponentInChildren<CharacterSprite>().sprite;
+                spriterinos[i] = spriterinos[i + 1];
+            }
+
+            else
+            {
+                characters[i].GetComponentInChildren<CharacterSprite>().sprite = spriterinos[5];
+                Debug.Log(spriten);
+                spriterinos[4] = spriterinos[5];
+                spriterinos[5] = spriten;
+            }
+        }
+
+        Debug.Log(spriterinos);
+        Debug.Log(characters);
+    }
+
+    public void RightButton1()
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (i == 0)
+            {
+                spriten = spriterinos[5];
+                Debug.Log(spriten);
+                // characters[0].GetComponentInChildren<CharacterSprite>().sprite = spriterinos[5];
+            }
+
+            if (i != characters.Length - 1)
+            {
+                characters[characters.Length - i - 1].GetComponentInChildren<CharacterSprite>().sprite = characters[characters.Length - i - 2].GetComponentInChildren<CharacterSprite>().sprite;
+                spriterinos[6 - i - 1] = spriterinos[6 - i - 2];
+                Debug.Log(spriten);
+                Debug.Log(characters[0].GetComponentInChildren<CharacterSprite>().sprite);
+            }
+
+            else
+            {
+                spriterinos[1] = spriterinos[0];
+                spriterinos[0] = spriten;
+
+                characters[0].GetComponentInChildren<CharacterSprite>().sprite = spriten;
+                Debug.Log(characters[0].GetComponentInChildren<CharacterSprite>().sprite);
+                Debug.Log(spriten);
+            }
+        }
+
+        Debug.Log(spriterinos);
+        Debug.Log(characters);
+
+    }
+
+    public void Confirm1()
+    {
+        //StaticForPlayer.idP1 = characters[2].GetInstanceID;
+    }
+
+    public void LeftButton2()
+    {
+        for (int i = 0; i < characters2.Length; i++)
+        {
+            if (i == 0)
+            {
+                spriten2 = spriterinos2[0];
+                Debug.Log(spriten2);
+            }
+
+            if (i != characters2.Length - 1)
+            {
+                characters2[i].GetComponentInChildren<CharacterSprite>().sprite = characters2[i + 1].GetComponentInChildren<CharacterSprite>().sprite;
+                spriterinos2[i] = spriterinos2[i + 1];
+            }
+
+            else
+            {
+                characters2[i].GetComponentInChildren<CharacterSprite>().sprite = spriterinos2[5];
+                Debug.Log(spriten2);
+                spriterinos2[4] = spriterinos2[5];
+                spriterinos2[5] = spriten2;
+            }
+        }
+
+        Debug.Log(spriterinos);
+        Debug.Log(characters);
+    }
+
+    public void RightButton2()
+    {
+      for (int i = 0; i < characters2.Length; i++)
+        {
+            if (i == 0)
+        {
+            spriten2 = spriterinos2[5];
+            Debug.Log(spriten2);
+            // characters[0].GetComponentInChildren<CharacterSprite>().sprite = spriterinos[5];
+        }
+
+        if (i != characters2.Length - 1)
+        {
+            characters2[characters2.Length - i - 1].GetComponentInChildren<CharacterSprite>().sprite = characters2[characters2.Length - i - 2].GetComponentInChildren<CharacterSprite>().sprite;
+            spriterinos2[6 - i - 1] = spriterinos2[6 - i - 2];
+            Debug.Log(spriten2);
+            Debug.Log(characters2[0].GetComponentInChildren<CharacterSprite>().sprite);
+        }
+
+        else
+        {
+            spriterinos2[1] = spriterinos2[0];
+            spriterinos2[0] = spriten2;
+
+            characters2[0].GetComponentInChildren<CharacterSprite>().sprite = spriten2;
+            Debug.Log(characters2[0].GetComponentInChildren<CharacterSprite>().sprite);
+            Debug.Log(spriten2);
+        }
+        }
+
+        Debug.Log(spriterinos2);
+        Debug.Log(characters2);
+     }
+
+    public void Confirm2()
     {
 
     }
---
+++ Assets/Scripts/CharacterSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CharacterSprite : MonoBehaviour
+{
+    public Sprite sprite;
+    void Start()
+    {
+        sprite = this.gameObject.GetComponent<Image>().sprite;
+    }
+
+    void Update()
+    {
+        this.gameObject.GetComponent<Image>().sprite = sprite;
+    }
+}
