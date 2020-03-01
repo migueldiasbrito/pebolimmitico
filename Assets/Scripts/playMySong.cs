c63f64a8ee008802a746c54213325830eae6b5aa
Moises <liricractos@gmail.com>
2020-03-01 02:51:53 +0000

    songScript e images

---
+++ Assets/Scripts/playMySong.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMySong : MonoBehaviour
+{
+
+    int oldStatp1;
+    int oldStatp2;
+
+    int statp1;
+    int statp2;
+
+    public AudioSource ads;
+    private void Start()
+    {
+        ads.PlayOneShot(ReturnToPlay(2));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(statp1 != oldStatp1 || statp2 != oldStatp2)
+        {
+            if(statp1 != oldStatp1)
+            {
+                oldStatp1 = statp1;
+            }
+            if (statp2 != oldStatp2)
+            {
+                oldStatp2 = statp2;
+            }
+
+        }
+        statp1 = StaticForPlayer.idP1;
+        statp2 = StaticForPlayer.idP2;
+    }
+
+
+
+    private AudioClip ReturnToPlay(int DeusInt)
+    {
+      
+        switch (DeusInt)
+        {
+            case 0:
+                return Resources.Load<AudioClip>("zeus/musica_zeus");
+             
+            case 1:
+                return Resources.Load<AudioClip>("ares/musica_ares");
+
+            case 2:
+                return Resources.Load<AudioClip>("Afrodite/musica_afrodite");
+
+            case 3:
+                return Resources.Load<AudioClip>("dioniso/musica_dioniso");
+
+            case 4:
+                return Resources.Load<AudioClip>("medusa/musica_medusa");
+
+            case 5:
+                return Resources.Load<AudioClip>("circe/musica_circe");
+
+
+            default: return Resources.Load<AudioClip>("Afrodite/musica_afrodite");
+
+
+        }
+
+
+
+
+
+    }
+}
